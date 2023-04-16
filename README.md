ab1749a13026a16bdd21f9ab88668dd35d88a23c
abouzarmofidian <a.mofidian69@gmail.com>
2023-04-16 11:52:11 +0330

    variable & math

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # bash-script
+ch0: Hello World
+ch1: Variables
+ch2: Basic Math
---
+++ ch01/variables.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+now=$(date)
+myname="Abouzar"
+myage="33"
+
+echo "The system time and date is:"
+
+echo $now
+
+echo "Hello, my name is $myname."
+
+echo "I'm $myage years old."
+
+echo "My username is: $USER"
+
+files=$(ls)
+
+echo $files
